d39f11642de22b4996c84bb19d20664488c37ba8
Suraj <77872306+DrSuraj@users.noreply.github.com>
2023-01-29 16:39:09 +0530

    Lower or Upper
    Given a character ch, check if it is an Uppercase letter or Lowercase letter. Return U if ch is an Uppercase letter and L if ch is Lowercase letter.
    
    Input Format
    First line contains a character ch
    
    Output Format
    Return the character L or U according to the question.
    
    Example 1
    Input
    k
    Output
    L
    
    Example 2
    Input
    K
    
    Output
    U

---
+++ 18_Lower or Upper.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static char LowerUpper(char ch) {
+        //Write your code here
+		if(ch >= 'A' && ch<='Z'){
+			return 'U';
+		}
+		else if(ch >= 'a' && ch<='z'){
+			return 'L';
+		}
+		return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        System.out.println(LowerUpper(ch));
+    }
+}
